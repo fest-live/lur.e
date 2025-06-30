569baac79848a39070a22d4d81c497c0689a7ac7
u2re-dev <c24b@u2re.ru>
2025-06-30 11:11:04 +0700

    getNode unline

--- src/lure/utils/DOM.ts
+++ src/lure/utils/DOM.ts
@@ -54,10 +54,10 @@ export const createElement = (selector): HTMLElement | DocumentFragment => {
 export const getNode = (E, mapper?: Function, index?: number) => {
     if (mapper) { return (E = getNode(mapper?.(E, index))); }
     if (E instanceof Node || E instanceof Text || E instanceof HTMLElement || E instanceof DocumentFragment) { return E; } else
-        if (typeof E?.value == "string" || typeof E?.value == "number") { return T(E); } else
-            if (typeof E == "function") { return getNode(E()); } else  // mapped arrays always empties after
-                if (typeof E == "string" || typeof E == "number") { return document.createTextNode(String(E)); } else
-                    if (typeof E == "object" && E != null) { return E?.element ?? elMap.get(E); }
+    if (typeof E?.value == "string" || typeof E?.value == "number") { return T(E); } else
+    if (typeof E == "function") { return getNode(E()); } else  // mapped arrays always empties after
+    if (typeof E == "string" || typeof E == "number") { return document.createTextNode(String(E)); } else
+    if (typeof E == "object" && E != null) { return E?.element ?? elMap.get(E); }
     return E;
 }
 
